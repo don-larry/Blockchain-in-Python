8961542ae9758f435be296da7ade07dcba488d55
don-larry <72495707+don-larry@users.noreply.github.com>
2023-02-08 09:55:45 +0100

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -1,7 +1,7 @@
 import hashlib
 
 
-class GoldCowry:
+class BlueCoin:
     def __init__(self, prev_block_hash, transaction_list):
         self.prev_block_hash = prev_block_hash
         self.transaction_list = transaction_list
@@ -10,23 +10,23 @@ class GoldCowry:
         self.blockHash = hashlib.sha256(self.blockData.encode()).hexdigest()
 
 
-t1 = "Tom sends 5 GC to Dan"
-t2 = "Dan sends 2 GC to Anna"
-t3 = "Anna sends 3.5 GC to Bruce"
-t4 = "Bruce sends 0.3 GC to Mary"
-t5 = "Mary sends 1.5 GC to Tom"
-t6 = "Anna send 3 GC to Dan"
+t1 = "Tom sends 5 BC to Dan"
+t2 = "Dan sends 2 BC to Anna"
+t3 = "Anna sends 3.5 BC to Bruce"
+t4 = "Bruce sends 0.3 BC to Mary"
+t5 = "Mary sends 1.5 BC to Tom"
+t6 = "Anna send 3 BC to Dan"
 
-initial_block = GoldCowry("Initial String", [t1, t2])
+initial_block = BlueCoin("Initial String", [t1, t2])
 
 print(initial_block.blockData)
 print(initial_block.blockHash)
 
-second_block = GoldCowry(initial_block.blockHash, [t3, t4])
+second_block = BlueCoin(initial_block.blockHash, [t3, t4])
 print(second_block.blockData)
 print(second_block.blockHash)
 
-third_block = GoldCowry(second_block.blockHash, [t5, t6])
+third_block = BlueCoin(second_block.blockHash, [t5, t6])
 print(third_block.blockData)
 print(third_block.blockHash)
 
